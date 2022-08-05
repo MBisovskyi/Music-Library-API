8aebc3a18ac2e529e0f6c550e12f0783be47307d
unknown <mbisovskyi@gmail.com>
2022-08-05 12:58:59 -0400

    GET API request method for songs_list functions is created

--- song/views.py
+++ song/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import SongSerializer
+from .models import Song
+from rest_framework import status
 
 # Create your views here.
+@api_view(['GET'], ['POST'])
+def songs_list(request):
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many = True)
+        return Response(serializer.data)
