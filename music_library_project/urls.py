04a5371dad1211a6afa992d509ad1351b2d9d526
unknown <mbisovskyi@gmail.com>
2022-08-05 12:36:39 -0400

    song app created + Song model created + settings fixed after music_library_project was renamed + Database migrated

--- music_library_project/urls.py
+++ music_library_project/urls.py
@@ -1,4 +1,4 @@
-"""music_library_api URL Configuration
+"""music_library_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
