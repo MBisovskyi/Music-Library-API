91fb07c55a7bc3c8faedf174f3c5ca85f9bfc445
unknown <mbisovskyi@gmail.com>
2022-08-05 13:50:30 -0400

    POST API request method is created

--- song/views.py
+++ song/views.py
@@ -12,3 +12,8 @@ def songs_list(request):
         songs = Song.objects.all()
         serializer = SongSerializer(songs, many = True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
